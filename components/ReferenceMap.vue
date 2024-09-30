83a4a39ab1556863c37c901754a537005e31b62b
Quentin GLX <quentin.glorieux@gmail.com>
2024-09-30 16:50:13 +0200

    first commit

---
+++ components/ReferenceMap.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="reference-map flex justify-center mx-auto">
+    <Image src="/images/map.png" alt="Reference Centres Map" class="block" />
+  </div>
+</template>
+
+<script setup></script>
